7ca73f877e2cd3d782bb31a63ab0634ff8655bc6
Anderson Pablo <anderson.pablo02@gmail.com>
2022-04-29 10:56:08 -0300

    feat(app.js):configuring local notifications

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as Notifications from 'expo-notifications';
 
 export default function App() {
+
+  function scheduleNotification() {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'My first local notification',
+        body: 'Pretty good!',
+        data: { userName: 'Max' }
+      },
+      trigger: {
+        seconds: 2
+      }
+    })
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+
+      <Button title='Schedule notification' onPress={scheduleNotification} />
       <StatusBar style="auto" />
     </View>
   );
