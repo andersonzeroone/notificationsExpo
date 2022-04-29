d3acdd773af4b0c1d60aa097361e502e51d4931a
Anderson Pablo <anderson.pablo02@gmail.com>
2022-04-29 11:24:30 -0300

    feat(app.js):configuring notification settings

--- App.js
+++ App.js
@@ -2,6 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import * as Notifications from 'expo-notifications';
 
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+      shouldShowAlert: true
+    };
+  }
+});
+
 export default function App() {
 
   function scheduleNotification() {
